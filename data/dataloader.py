b140596eec94e0d764e1aa0b573b33605aa79fed
Thomas Ghorbanian <64096052+thomasgho@users.noreply.github.com>
2021-06-12 23:26:29 +0100

    Update dataloader.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,57 +1,37 @@
+import pickle
 import torch
-import numpy as np
-from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
 
 
-# define auxiliary dataset class
-class ndarrayDataset(Dataset):
-    """simple dataset"""
-
-    def __init__(self, X, y_structure, y_energy):
-        super(ndarrayDataset, self).__init__()
-        self.X = torch.from_numpy(X).float()
-        self.y_structure = torch.from_numpy(y_structure).float()
-        self.y_energy = torch.from_numpy(y_energy).long()
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, X): #, y):
+        super(Dataset, self).__init__()
+        self.X = X
+        #self.y = y
 
     def __len__(self):
-        return self.X.shape[0]
+        return len(self.X)
 
     def __getitem__(self, idx):
-        return self.X[idx], self.y_structure[idx], self.y_energy[idx]
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        return self.X[idx]#, self.y[idx]
 
 
-# define material dataloader
-def material_loader(data_loc, batch_size):
-    '''
-    returns one train set and two validation sets (ratio 60:20:20)
-    '''
+def dataloader(rootdir, batch_size):
 
-    # load data
-    data = np.load(data_loc, allow_pickle=True)
+    with open(rootdir, 'rb') as f:
+        dataset = pickle.load(f)
 
-    # attributes
-    id = data[:,0]
-    atom_type = data[:,1]
-    xrd = data[:,2:3602]
-    space_group = (data[:,3602]-1).astype(int)
-    band_gap = data[:,3603]
-    energy = data[:,3604]
-    mag_moment = data[:,3605]
-    energy_above_hull = data[:,3606]
-    bravais = (data[:,3607]-1)
-    targets = data[:,3602:]
+    dataset = torch.unsqueeze(torch.as_tensor(dataset), 1)
+    dataset = Dataset(dataset)
 
-    # split train and test data
-    X_train, X_test, energy_train, energy_test, bravais_train, bravais_test = train_test_split(xrd, energy, bravais, test_size=0.40, shuffle=True, random_state=9)
-    X_val_1, X_val_2, energy_val_1, energy_val_2, bravais_val_1, bravais_val_2 = train_test_split(X_test, energy_test, bravais_test, test_size=0.50, shuffle=True, random_state=9)
-    train_dataset = ndarrayDataset(X_train, energy_train, bravais_train)
-    val_dataset_1 = ndarrayDataset(X_val_1, energy_val_1, bravais_val_1)
-    val_dataset_2 = ndarrayDataset(X_val_2, energy_val_2, bravais_val_2)
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    trainset, testset = torch.utils.data.random_split(dataset, [train_size, test_size])
 
-    # pytorch dataloader
-    train_loader = DataLoader(train_dataset, batch_size=batch_size)
-    val_loader_1 = DataLoader(val_dataset_1, batch_size=batch_size)
-    val_loader_2 = DataLoader(val_dataset_2, batch_size=batch_size)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size)
 
-    return train_loader, val_loader_1, val_loader_2
+    return trainloader, testloader
