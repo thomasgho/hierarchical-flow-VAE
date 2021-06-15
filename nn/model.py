bdab4e0763d28c317153175cecf1e0d54d4b0e0d
Thomas Ghorbanian <64096052+thomasgho@users.noreply.github.com>
2021-06-15 19:25:48 +0100

    Update model.py

--- nn/model.py
+++ nn/model.py
@@ -5,11 +5,11 @@ from nn.flow import *
 
 
 class VAE(nn.Module):
-    def __init__(self, input_dim, feat_dims=[32, 64], z_dims=[15, 15]):
+    def __init__(self, input_dim, feat_dims=[32, 64], z_dims=[15, 15], trace_estimator='hutchinson'):
         super(VAE, self).__init__()
         self.encoder = Encoder(input_dim, feat_dims, z_dims)
         self.decoder = Decoder(input_dim, list(reversed(feat_dims)), list(reversed(z_dims)))
-        self.flow = nn.ModuleList([NeuralDE(Flow(z), solver='dopri5', sensitivity='adjoint') for z in z_dims])
+        self.flow = nn.ModuleList([NeuralDE(Flow(z, trace_estimator=trace_estimator), solver='dopri5', sensitivity='adjoint') for z in z_dims])
         self.augment = Augmenter(augment_idx=1, augment_dims=1)
 
     def forward(self, x):
@@ -39,6 +39,7 @@ def test():
 
 if __name__ == "__main__":
     test()
+
 
 
 
