ec0341ae88f10f257e2a1c3f6823f595e649aeca
Thomas Ghorbanian <64096052+thomasgho@users.noreply.github.com>
2022-01-08 17:14:50 +0000

    Update model.py

--- nn/model.py
+++ nn/model.py
@@ -8,7 +8,7 @@ class VAE(nn.Module):
     def __init__(self, input_dim, feat_dims=[32, 64], z_dims=[15, 15], trace_estimator='hutchinson'):
         super(VAE, self).__init__()
         self.encoder = Encoder(input_dim, feat_dims, z_dims)
-        self.decoder = Decoder(input_dim, list(reversed(feat_dims)), list(reversed(z_dims)))
+        self.decoder = Decoder([1, input_dim[1]], list(reversed(feat_dims)), list(reversed(z_dims)))
         self.flow = nn.ModuleList([NeuralDE(Flow(z, trace_estimator=trace_estimator), solver='dopri5', sensitivity='adjoint') for z in z_dims])
         self.augment = Augmenter(augment_idx=1, augment_dims=1)
 
@@ -23,26 +23,18 @@ class VAE(nn.Module):
             z_flows.append(z)
             trJs.append(trJ)
 
-        ###################
-        # SE(3) TRANSFORM #
-        ###################
-
         x_recon = self.decoder(z_flows)
         return x_recon, mu_levels, var_levels, z_flows, trJs
 
 
 def test():
-    x = torch.randn((3,1,100))
-    model = VAE(input_dim=100, z_dims=[15, 15], feat_dims=[32, 64])
+    x = torch.randn((3,8,1000))
+    model = VAE(input_dim=[8,1000], z_dims=[15, 15], feat_dims=[32, 64])
     x_recon, mu_levels, var_levels, z_flows, trJs = model(x)
     print(x_recon.shape)
 
 if __name__ == "__main__":
     test()
-
-
-
-
 
 
 
